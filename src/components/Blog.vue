59e91178a8c2b041a8a841f8dc09f5812ecc8114
Mario Leone <mleone10@gmail.com>
2019-11-27 20:34:41 -0500

    Adding blog archive styling.

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,5 +1,11 @@
 <template>
-        <div>Hello there!</div>
+        <div class="blog">
+                <ul>
+                        <li><router-link to="/">Entry 1</router-link></li>
+                        <li>Entry 2</li>
+                        <li>Entry 3</li>
+                </ul>
+        </div>
 </template>
 
 <script>
@@ -9,6 +15,29 @@ export default {
 </script>
 
 <style scoped>
+ul {
+        padding-left: 1.4em;
+}
+
+li {
+        font-size: 1.2em;
+        margin: 1em 0em;
+        font-family: 'Roboto', sans-serif;
+}
+
+a {
+        text-decoration: none;
+        color: #FFF6E9;
+}
+
 @media only screen and (min-width:900px) {
+        ul {
+                margin: unset;
+                padding-left: 1em;
+        }
+
+        li {
+                font-size: 0.5em;
+        }
 }
 </style>
